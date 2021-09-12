c4c58e0ab1adffc333454e91d249ab6d3707d76d
StrongBeauty <lonely__wind@mail.ru>
2021-09-12 23:57:29 +0300

    dragndrop project

--- src/Projects/Projects.js
+++ src/Projects/Projects.js
@@ -9,6 +9,7 @@ import TDLImage from "./../assets/image/h9-port-img-5.jpg"
 import CounterImage from "./../assets/image/port-gallery-img-6.jpg"
 import CurrencyImage from "./../assets/image/port-gallery-img-10.jpg"
 import SliderImage from "./../assets/image/h9-port-img-1.jpg"
+import DragDropImage from "./../assets/image/blog-post-6.jpg"
 
 
 export const Projects = () => {
@@ -27,6 +28,9 @@ export const Projects = () => {
     const slider = {
         backgroundImage: `url(${SliderImage})`,
     }
+    const dragDrop = {
+        backgroundImage: `url(${DragDropImage})`,
+    }
 
     return (
 
@@ -41,6 +45,7 @@ export const Projects = () => {
                     <Project style={todolist} title={'ToDoList'} description={'There will be a description of my project soon'} href={''}/>
                     <Project style={slider} title={'Slider'} description={'There will be a description of my project soon'} href={'https://strongbeauty.github.io/CardsSources/'}/>
                     <Project style={counter} title={'Counter'} description={'There will be a description of my project soon'} href={''}/>
+                    <Project style={dragDrop} title={'Drag & Drop'} description={'There will be a description of my project soon'} href={'https://strongbeauty.github.io/DragnDrop/'}/>
                     <Project style={tdl} title={'Currency'} description={'There will be a description of my project soon'} href={''}/>
                 </div>
 
